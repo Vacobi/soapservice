13caf0a03927ff5095a4fba0716c918c20930c68
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-03-02 20:16:36 +0300

    Initial commit

---
+++ soapservice/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "axiomatika.converter"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.ws:spring-ws-core")
+	implementation("org.springframework.boot:spring-boot-starter-web-services")
+	implementation("wsdl4j:wsdl4j")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+	/**
+	 * XSLT
+	 */
+	implementation("org.springframework:spring-oxm")
+
+	/**
+	 * Tests
+	 */
+	testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+	testImplementation("org.assertj:assertj-core:3.24.2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ soapservice/src/test/java/axiomatika/converter/soapservice/SoapserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package axiomatika.converter.soapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
