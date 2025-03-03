ee44211c9897b4a061f0816c89847a633bf76946
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-03-03 19:14:28 +0300

    fix: endpoint gets requests from RESTservice

--- soapservice/build.gradle.kts
+++ soapservice/build.gradle.kts
@@ -18,22 +18,22 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.ws:spring-ws-core:3.1.8")
-	implementation("org.springframework.boot:spring-boot-starter-web-services:3.3.5")
-	implementation("wsdl4j:wsdl4j")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
-	/**
-	 * XSLT
-	 */
-	implementation("org.springframework:spring-oxm")
-
-	/**
-	 * Tests
-	 */
-	testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
-	testImplementation("org.assertj:assertj-core:3.24.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	dependencies {
+		implementation("org.springframework.ws:spring-ws-core") {
+			exclude(group ="javax.xml.soap", module = "javax.xml.soap-api")
+		}
+		implementation("org.springframework.boot:spring-boot-starter-web-services") {
+			exclude(group = "javax.xml.soap", module = "javax.xml.soap-api")
+		}
+		implementation("wsdl4j:wsdl4j")
+		implementation("jakarta.xml.soap:jakarta.xml.soap-api:3.0.2")
+		implementation("com.sun.xml.messaging.saaj:saaj-impl:3.0.4")
+		implementation("org.springframework:spring-oxm")
+		annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+		testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+		testImplementation("org.assertj:assertj-core:3.24.2")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+	}
 }
 
 tasks.withType<Test> {
--- soapservice/src/main/java/axiomatika/converter/soapservice/config/SoapWebServiceConfig.java
+++ soapservice/src/main/java/axiomatika/converter/soapservice/config/SoapWebServiceConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 
 @EnableWs
@@ -17,5 +18,10 @@ public class SoapWebServiceConfig {
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
         return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean
+    public SaajSoapMessageFactory messageFactory() {
+        return new SaajSoapMessageFactory();
     }
 }
