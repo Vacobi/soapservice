61f4e98a774061ad9a47554bd0a9e506bdda376a
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-03-05 19:22:14 +0300

    delete: removed unused dependencies

--- soapservice/build.gradle.kts
+++ soapservice/build.gradle.kts
@@ -19,17 +19,13 @@ repositories {
 
 dependencies {
 	dependencies {
-		implementation("org.springframework.ws:spring-ws-core") {
-			exclude(group ="javax.xml.soap", module = "javax.xml.soap-api")
-		}
-		implementation("org.springframework.boot:spring-boot-starter-web-services") {
-			exclude(group = "javax.xml.soap", module = "javax.xml.soap-api")
-		}
-		implementation("wsdl4j:wsdl4j")
-		implementation("jakarta.xml.soap:jakarta.xml.soap-api:3.0.2")
-		implementation("com.sun.xml.messaging.saaj:saaj-impl:3.0.4")
-		implementation("org.springframework:spring-oxm")
+		implementation("org.springframework.ws:spring-ws-core")
+		implementation("org.springframework.boot:spring-boot-starter-web-services")
 		annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+		/**
+		 * Tests
+		 */
 		testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
 		testImplementation("org.assertj:assertj-core:3.24.2")
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
